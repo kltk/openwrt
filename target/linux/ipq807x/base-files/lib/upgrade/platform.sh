30877f9211d8b47a7480dd4fa6b72956c961e4ca
Robert Marko <robimarko@gmail.com>
2021-05-19 21:04:43 +0200

    ipq807x: add Xiaomi AX3600
    Xiaomi AX3600 is a budget 802.11ax dual-band router/AP.
    Specifications:
    * CPU: Qualcomm IPQ8071A Quad core Cortex-A53 1.4GHz
    * RAM: 512MB of DDR3
    * Storage: 256MB of parallel NAND
    * Ethernet: 4x1G RJ45 ports (QCA8075) with 1x status LED per port
    * WLAN:
    	* PCI based Qualcomm QCA9889 1x1 802.11ac Wawe 2 for IoT
    	* 2.4GHz: Qualcomm QCN5024 2x2 802.11b/g/n/ax 574 Mbps PHY rate
    	* 5GHz: Qualcomm QCN5054 4x4@80MHz or 2x2@160MHz 802.11a/b/g/n/ac/ax 2402 PHY rate
    * LED-s:
    	* System (Blue and Yellow)
    	* IoT (Blue)
    	* Network (Blue and Yellow)
    * Buttons: 1x Soft reset
    * Power: 12V DC Jack
    
    Installation instructions:
    
    Obtaining SSH access is mandatory
    https://openwrt.org/inbox/toh/xiaomi/xiaomi_ax3600#obtain_ssh_access
    
    Installation is done by the ubiformat method, through SSH:
    1. Open an SSH shell to the router
    2. Copy the file openwrt-ipq807x-generic-xiaomi_ax3600-initramfs-factory.ubi to the /tmp directory
    3. Check which rootfs partition is your router booted in (0 = rootfs | 1 = rootfs_1):
    nvram get flag_boot_rootfs
    
    4. Find the rootfs and rootfs_1 mtd indexes respectively:
    cat /proc/mtd
    Please confirm if mtd12 and mtd13 are the correct indexes from above!
    
    5. Use the command ubiformat to flash the opposite mtd with UBI image:
    
    If nvram get flag_boot_rootfs returned 0:
    ubiformat /dev/mtd13 -y -f /tmp/openwrt-ipq807x-generic-xiaomi_ax3600-initramfs-factory.ubi -s 2048 -O 2048 && nvram set flag_boot_rootfs=1 && nvram set flag_last_success=1 && nvram commit
    
    otherwise:
    ubiformat /dev/mtd12 -y -f /tmp/openwrt-ipq807x-generic-xiaomi_ax3600-initramfs-factory.ubi -s 2048 -O 2048 && nvram set flag_boot_rootfs=0 && nvram set flag_last_success=0 && nvram commit
    
    6. Reboot the device by:
    reboot
    
    Previous commands flashed an ubinized OpenWrt initramfs that will serve as the intermediate step
    since OpenWrt uses unified rootfs in order to fully utilize NAND and provide enough space for packages.
    Continue in order to pernamently flash OpenWrt:
    
    7. SSH into OpenWrt from one of the LAN ports
    8. Copy the file openwrt-ipq807x-generic-xiaomi_ax3600-squashfs-sysupgrade.bin to the /tmp directory
    9. Sysupgrade the device:
    sysupgrade -n /tmp/openwrt-ipq807x-generic-xiaomi_ax3600-squashfs-sysupgrade.bin
    
    Device will reboot with OpenWrt, and then sysupgrade can be used to upgrade the device when desired.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq807x/base-files/lib/upgrade/platform.sh
@@ -4,61 +4,57 @@ REQUIRE_IMAGE_METADATA=1
 RAMFS_COPY_BIN='fw_printenv fw_setenv'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
+xiaomi_initramfs_prepare() {
+	# Wipe UBI if running initramfs
+	[ "$(rootfs_type)" = "tmpfs" ] || return 0
+
+	local rootfs_mtdnum="$( find_mtd_index rootfs )"
+	if [ ! "$rootfs_mtdnum" ]; then
+		echo "unable to find mtd partition rootfs"
+		return 1
+	fi
+
+	local kern_mtdnum="$( find_mtd_index ubi_kernel )"
+	if [ ! "$kern_mtdnum" ]; then
+		echo "unable to find mtd partition ubi_kernel"
+		return 1
+	fi
+
+	ubidetach -m "$rootfs_mtdnum"
+	ubiformat /dev/mtd$rootfs_mtdnum -y
+
+	ubidetach -m "$kern_mtdnum"
+	ubiformat /dev/mtd$kern_mtdnum -y
+}
+
 platform_check_image() {
 	return 0;
 }
 
+platform_pre_upgrade() {
+	case "$(board_name)" in
+	xiaomi,ax3600)
+		xiaomi_initramfs_prepare
+		;;
+	esac
+}
+
 platform_do_upgrade() {
 	case "$(board_name)" in
-	dynalink,dl-wrx36)
-		nand_do_upgrade "$1"
-		;;
-	edgecore,eap102)
-		active="$(fw_printenv -n active)"
-		if [ "$active" -eq "1" ]; then
-			CI_UBIPART="rootfs2"
-		else
-			CI_UBIPART="rootfs1"
-		fi
-		# force altbootcmd which handles partition change in u-boot
-		fw_setenv bootcount 3
-		fw_setenv upgrade_available 1
-		nand_do_upgrade "$1"
-		;;
-	edimax,cax1800)
-		nand_do_upgrade "$1"
-		;;
-	qnap,301w)
-		kernelname="0:HLOS"
-		rootfsname="rootfs"
-		mmc_do_upgrade "$1"
-		;;
-	redmi,ax6|\
-	xiaomi,ax3600|\
-	xiaomi,ax9000)
-		part_num="$(fw_printenv -n flag_boot_rootfs)"
-		if [ "$part_num" -eq "0" ]; then
-			CI_UBIPART="rootfs_1"
-			target_num=1
-			# Reset fail flag for the current partition
-			# With both partition set to fail, the partition 2 (bit 1)
-			# is loaded
-			fw_setenv flag_try_sys2_failed 0
-		else
-			CI_UBIPART="rootfs"
-			target_num=0
-			# Reset fail flag for the current partition
-			# or uboot will skip the loading of this partition
-			fw_setenv flag_try_sys1_failed 0
-		fi
-
-		# Tell uboot to switch partition
-		fw_setenv flag_boot_rootfs $target_num
-		fw_setenv flag_last_success $target_num
-
-		# Reset success flag
+	xiaomi,ax3600)
+		# Enforce single partition.
+		fw_setenv flag_boot_rootfs 0
+		fw_setenv flag_last_success 0
 		fw_setenv flag_boot_success 0
+		fw_setenv flag_try_sys1_failed 0
 
+		# Second partition won't work and can't be used
+		# Flag it as failed by default
+		fw_setenv flag_try_sys2_failed 1
+
+		# Kernel and rootfs are placed in 2 different UBI
+		CI_KERN_UBIPART="ubi_kernel"
+		CI_ROOT_UBIPART="rootfs"
 		nand_do_upgrade "$1"
 		;;
 	*)
