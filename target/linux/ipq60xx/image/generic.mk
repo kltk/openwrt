9d7115bc752cefffbc99e012fdc3a85b2dfb13b4
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2022-08-07 00:30:23 -0500

    [WIP] ipq60xx: add Qualcomm Atheros IPQ60xx target

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -30,32 +30,3 @@ define Device/cp01-c1
 	SOC := ipq6018
 endef
 TARGET_DEVICES += cp01-c1
-
-define Device/tplink_eap610-outdoor
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := TP-Link
-	DEVICE_MODEL := EAP610-Outdoor
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	SOC := ipq6018
-	IMAGE/nand-factory.ubi := append-ubi | tplink-image-2022
-	TPLINK_SUPPORT_STRING := SupportList: \
-		EAP610-Outdoor(TP-Link|UN|AX1800-D):1.0 \
-		EAP610-Outdoor(TP-Link|JP|AX1800-D):1.0 \
-		EAP610-Outdoor(TP-Link|CA|AX1800-D):1.0
-endef
-TARGET_DEVICES += tplink_eap610-outdoor
-
-define Device/linksys_mr7350
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := Linksys
-	DEVICE_MODEL := MR7350
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	SOC := ipq6018
-	DEVICE_PACKAGES := kmod-leds-pca963x \
-		kmod-usb3 kmod-usb-dwc3 kmod-usb-dwc3-qcom
-endef
-TARGET_DEVICES += linksys_mr7350
