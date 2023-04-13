c17d9f0c87eb5af38538217fb1c71cf66dd01e08
solidus1983 <hacktrix2006@gmail.com>
2023-04-13 13:27:17 +0000

    Trying to fix mtdsplit

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -18,6 +18,7 @@ endef
 
 define Device/UbiFit
 	KERNEL_IN_UBI := 1
+	FILESYSTEMS := squashfs
 	IMAGES := nand-factory.ubi nand-sysupgrade.bin
 	IMAGE/nand-factory.ubi := append-ubi
 	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
@@ -35,7 +36,6 @@ TARGET_DEVICES += cp01-c1
 define Device/glinet_gl-ax1800
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	FILESYSTEMS := squashfs
 	DEVICE_VENDOR := GL-iNet
 	DEVICE_MODEL := GL-AX1800
 	BLOCKSIZE := 128k
@@ -51,7 +51,6 @@ TARGET_DEVICES += glinet_gl-ax1800
 define Device/glinet_gl-axt1800
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	FILESYSTEMS := squashfs
 	DEVICE_VENDOR := GL-iNet
 	DEVICE_MODEL := GL-AXT1800
 	BLOCKSIZE := 128k
