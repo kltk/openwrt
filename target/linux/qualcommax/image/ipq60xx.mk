858de810dd8ae8d67819244e937d5639f98244fc
Dirk Buchwalder <buchwalder@posteo.de>
2023-06-02 21:04:39 +0200

    ipq60xx: add basic support for netgear wax214
        Netgear WAX214 is a 802.11 ax dual-band AP
        with PoE. (similar to Engenius EWS357APV3)
    
        Specifications:
    
            •     CPU: Qualcomm IPQ6010 Quad core Cortex-A53
            •     RAM: 512MB of DDR3
            •     Storage: 128MB NAND (Macronix MX30UF1G18AC)
            •     Ethernet: 1x 1G RJ45 port (QCA8072) PoE
            •     WIFI:
                  2.4GHz: Qualcomm QCN5022 2x2 802.11b/g/n/ax 574 Mbps PHY rate
                  5GHz: Qualcomm QCN5052 2x2 802.11a/b/g/n/ac/ax 1201 PHY rate
    
            •     LEDs:
                  4 x GPIO-controlled LEDs
                    - 1 Power LED (orange)
                    - 1 LAN LED (blue)
                    - 1 WIFI 5g LED (blue)
                    - 1 WIFI 2g LED (blue)
                    black_small_square  Buttons: 1x soft reset
                    black_small_square  Power: 12V DC jack or PoE (802.3af )
    
                An populated serial header is onboard, format is
                 1.25mm 4p (DF13A-4P-1.25H)
                RX/TX is working, bootwait is active, secure boot is not
                enabled.
    
                The root password of the stock firmware is unknown,
                but failsafe mode can be entered to reset the password.
    
                Installation Instructions:
    
                    - obtain serial access
                    - stop auto boot (press "4", Entr boot command line
    		  interface)
                    - setenv active_fw 0 (to boot from the primary rootfs,
                      or set to 1 to boot from the secondary rootfs
                      partition)
                    - saveenv
    
                    - tftpboot the initramfs image
                    - bootm
    
                    - copy
    		  openwrt-ipq60xx-generic-netgear_wax214-squashfs-nand-factory.ubi
                      to the device
                    - write the image to the NAND:
                       - cat /proc/mtd and look for rootfs partition (should
    		     be mtd11,
                         or mtd12 if you choose active_fw 1)
                       - ubiformat /dev/mtd0 -f -y
    		     openwrt-ipq60xx-generic-netgear_wax214
                         -squashfs-nand-factory.ubi
                    - reboot
    
                Note: the firmware is senao-based. But I was unable to build
                      a valid senao-header into the image.
                      Maybe they changed the header format and senaoFW isn't
                      working any more.
    
    Signed-off-by: Dirk Buchwalder <buchwalder@posteo.de>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -9,3 +9,16 @@ define Device/linksys_mr7350
        DEVICE_PACKAGES := ipq-wifi-linksys_mr7350
 endef
 TARGET_DEVICES += linksys_mr7350
+
+define Device/netgear_wax214
+       $(call Device/FitImage)
+       $(call Device/UbiFit)
+       DEVICE_VENDOR := Netgear
+       DEVICE_MODEL := WAX214
+       BLOCKSIZE := 128k
+       PAGESIZE := 2048
+       DEVICE_DTS_CONFIG := config@cp03-c1
+       SOC := ipq6018
+       DEVICE_PACKAGES := ipq-wifi-netgear_wax214
+endef
+TARGET_DEVICES += netgear_wax214
--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -10,7 +10,8 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
-	linksys,mr7350)
+	linksys,mr7350|\
+	netgear,wax214)
 		nand_do_upgrade "$1"
 		;;
 	*)
