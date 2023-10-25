636c1fb37681542f5359c76a2b1c12d9dcbf9c8b
z <haig8@msn.com>
2023-10-25 22:33:10 +0800

    update

--- build.py
+++ build.py
@@ -44,7 +44,7 @@ def useExternalToolchain():
     os.path.join(__abs_dir__, '../../toolchain.tar.xz')
   ]
   for p in paths:
-    grouprun(['ls -al', p])
+    grouprun(['ls', '-al', p])
   for p in paths:
     if os.path.exists(p):
       grouprun(['./scripts/ext-toolchain.sh', '--toolchain', p, '--overwrite-config', '--config qualcommax/ipq60xx'])
